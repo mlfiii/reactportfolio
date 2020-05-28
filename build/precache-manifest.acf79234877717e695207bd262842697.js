da4c706f606c13cac0696623fd3dd9bc0b5bdf4f
Vero Verto <gin@luthercorp.com>
2020-05-27 17:39:11 -0700

    Build adjustments.

---
+++ build/precache-manifest.acf79234877717e695207bd262842697.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19fdee4549ff309735f99cd0e058a83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de0e3215cb97547468d7",
+    "url": "/static/css/2.afd40c00.chunk.css"
+  },
+  {
+    "revision": "8386ff324c65c642da18",
+    "url": "/static/css/main.26acdd1c.chunk.css"
+  },
+  {
+    "revision": "de0e3215cb97547468d7",
+    "url": "/static/js/2.0dfe1980.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0dfe1980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8386ff324c65c642da18",
+    "url": "/static/js/main.42f97203.chunk.js"
+  },
+  {
+    "revision": "2490790c3d9e824a7faa",
+    "url": "/static/js/runtime-main.18bbefb9.js"
+  },
+  {
+    "revision": "31ea663f7c1ee05349d3ad19a6876ad6",
+    "url": "/static/media/CodeQuiz.31ea663f.png"
+  },
+  {
+    "revision": "6f26b05b0ca65991a5be0adb3d72bd0b",
+    "url": "/static/media/PasswordGenerator.6f26b05b.png"
+  },
+  {
+    "revision": "dc9e562e28e8b15cd987a92c41079fd1",
+    "url": "/static/media/TheNewsStand.dc9e562e.png"
+  },
+  {
+    "revision": "e625ad3a35b67b9b7ff4a47b1ebf468e",
+    "url": "/static/media/TrackSnax.e625ad3a.png"
+  },
+  {
+    "revision": "c760d9ba081300c66395533a173b9d62",
+    "url": "/static/media/WeatherDashboard.c760d9ba.png"
+  },
+  {
+    "revision": "975f7b392604096c4cc44301a0dc61a8",
+    "url": "/static/media/fabrizio-conti-kXVogATbFgA-unsplash.975f7b39.jpg"
+  },
+  {
+    "revision": "0ddccb15072df5deceffe6f0e2af90ec",
+    "url": "/static/media/luke-chesser-hQo6Uyo4nBg-unsplash.0ddccb15.jpg"
+  },
+  {
+    "revision": "01f7d1e12262471547fe3e31c7791cdb",
+    "url": "/static/media/mlfiii.01f7d1e1.jpeg"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/static/media/white-waves.a06a3248.png"
+  }
+]);
